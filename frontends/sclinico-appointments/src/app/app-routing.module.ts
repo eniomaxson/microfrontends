3355188059e99a5f07045879ba19e216ce512dfa
Enio Maxson <enio.maxson@gmail.com>
2022-11-30 23:26:22 +0000

    adapting the appointments to use auth shared module as an inbrowser module

--- frontends/sclinico-appointments/src/app/app-routing.module.ts
+++ frontends/sclinico-appointments/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: EmptyRouteComponent, path: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {
+      provide: APP_BASE_HREF,
+      useValue: '/appointments',
+    },
+  ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontends/sclinico-appointments/src/app/app.module.ts
+++ frontends/sclinico-appointments/src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountStorageManager } from '@spms/shared';
+import { ACCOUNT_STORAGE } from './account.storage.provider';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [
+    {
+      provide: ACCOUNT_STORAGE,
+      useClass: AccountStorageManager,
+      useFactory: () => new AccountStorageManager(),
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
